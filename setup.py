06d2991c58cd18cd7f58ea18447d8b23413a884f
Jueri <jueri.keller@smail.th-koeln.de>
2022-03-05 10:44:49 +0100

    add test action

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     include_package_data=True,
-    # install_requires=["Click", "requests"],
-    # entry_points={
-    #     "console_scripts": [
-    #         "BibTexTools = BibTexTools.clean_bibtex:clean_bibtex",
-    #     ],
-    # },
+    install_requires=["Click", "requests"],
+    entry_points={
+        "console_scripts": [
+            "BibTexTools = BibTexTools.CLI:cli",
+        ],
+    },
 )
